b3c1ca18442248a6fb84b8d251f8e12eeecf7ee1
Bart Busschots <bart.busschots@nuim.ie>
2017-10-18 13:24:51 +0100

    Improved Error tracking
    Added a custom error class for Moodle Exceptions and added detection of exceptions into .submit().
    
    Documentation needs to be revised before releasing as next version.

--- test/test.js
+++ test/test.js
@@ -9,9 +9,48 @@ const MoodleWSClient = require('../');
 const validateParams = require('@maynoothuniversity/validate-params');
 const validate = validateParams.validateJS();
 
+// import modules needed for reading the optional Moodle instance details
+const path = require('path');
+const fs = require('fs-extra');
+
 //
 //=== Utility Variables & Functions ============================================
 //
+
+/**
+ * An object which may be populated with Moodle instance details for running
+ * tests against a real Moodle instance.
+ *
+ * This object is populated from `test/moodleDetails.json`.
+ *
+ * @type {{url: string, token: string}}
+ */
+const MOODLE_DETAILS = {
+    url: '',
+    token: ''
+};
+
+/**
+ * The path to search for the Moodle details JSON file at.
+ * @type {string}
+ */
+const MOODLE_DETAILS_PATH = path.join(__dirname, 'moodleDetails.json');
+try{
+    if(fs.existsSync(MOODLE_DETAILS_PATH)){
+        let moodleDetails = fs.readJsonSync(MOODLE_DETAILS_PATH);
+        if(moodleDetails.url && moodleDetails.token){
+            MOODLE_DETAILS.url = moodleDetails.url;
+            MOODLE_DETAILS.token = moodleDetails.token;
+            console.info(`will run live tests against Moodle instance at ${MOODLE_DETAILS.url} with token=${MOODLE_DETAILS.token}`);
+        }else{
+            console.info(`skipping live tests - invalid data in Moodle details file at '${MOODLE_DETAILS_PATH}'`);
+        }
+    }else{
+        console.info(`skipping live tests - no Moodle details file at '${MOODLE_DETAILS_PATH}'`);
+    }
+}catch(err){
+    console.warn(`failed to load moodle details from '${MOODLE_DETAILS_PATH}' with error: ${err.message}`);
+}
 
 /**
  * An object containing dummy data. The pieces of dummy data are indexed by
@@ -22,7 +61,7 @@ const validate = validateParams.validateJS();
  * 
  * @type {Object.<string, {desc: string, val: *}>}
  */
-var DUMMY_DATA = {};
+let DUMMY_DATA = {};
 
 /**
  * An object just like {@link DUMMY_DATA}, but limited to just the basic types
@@ -30,7 +69,7 @@ var DUMMY_DATA = {};
  * 
  * @see DUMMY_DATA
  */
-var DUMMY_BASIC_TYPES = {};
+let DUMMY_BASIC_TYPES = {};
 
 // add a callback to reset the dummy data before each test
 QUnit.testStart(function() {
@@ -153,109 +192,187 @@ function dummyBasicTypesExcept(){
 //=== Define Tests =============================================================
 //
 
-QUnit.test('class exists', function(a){
-    a.equal(typeof MoodleWSClient, 'function');
-});
-    
-QUnit.module('constructor', {}, function(){
-    QUnit.test('argument handling', function(a){
-        a.expect(7);
+//
+//--- Off-line tests (always run) ----------------------------------------------
+//
+
+QUnit.module('main exported class', {}, function(){
+
+    QUnit.test('class exists', function(a){
+        a.equal(typeof MoodleWSClient, 'function');
+    });
         
-        // make sure required arguments are required
-        a.throws(
-            function(){
-                new MoodleWSClient();
-            },
-            validateParams.ValidationError,
-            'call without any arguments throws error'
-        );
-        a.throws(
-            function(){
-                new MoodleWSClient(dummyVal('url'));
-            },
-            validateParams.ValidationError,
-            'call with only one valid argument throws error'
-        );
+    QUnit.module('constructor', {}, function(){
+        QUnit.test('argument handling', function(a){
+            a.expect(7);
+            
+            // make sure required arguments are required
+            a.throws(
+                function(){
+                    new MoodleWSClient();
+                },
+                validateParams.ValidationError,
+                'call without any arguments throws error'
+            );
+            a.throws(
+                function(){
+                    new MoodleWSClient(dummyVal('url'));
+                },
+                validateParams.ValidationError,
+                'call with only one valid argument throws error'
+            );
+            
+            // make sure valid arguments are accepted
+            a.ok(
+                (function(){
+                    new MoodleWSClient(dummyVal('url'), dummyVal('token'));
+                    return true;
+                })(),
+                'call with all required valid arguments does not throw an error'
+            );
+            
+            // make sure arguments are properly saved into the constructed object
+            var dummyOptions = {
+                acceptUntrustedTLSCert: true,
+                dataFormat: 'xml',
+                timeout: 10000
+            };
+            var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'), dummyOptions);
+            a.equal(m1._moodleUrl, dummyVal('url'), 'Moodle base URL stored');
+            a.equal(m1._token, dummyVal('token'), 'webservice token stored');
+            a.deepEqual(m1._options, dummyOptions, 'options stored');
+            
+            // check URL coercion
+            var m2 = new MoodleWSClient('https://localhost', dummyVal('token'));
+            a.equal(m2._moodleUrl,'https://localhost/', 'trailing / coerced onto Moodle base URL');
+        });
         
-        // make sure valid arguments are accepted
-        a.ok(
-            (function(){
-                new MoodleWSClient(dummyVal('url'), dummyVal('token'));
-                return true;
-            })(),
-            'call with all required valid arguments does not throw an error'
-        );
-        
-        // make sure arguments are properly saved into the constructed object
-        var dummyOptions = {
-            acceptUntrustedTLSCert: true,
-            dataFormat: 'xml',
-            timeout: 10000
-        };
-        var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'), dummyOptions);
-        a.equal(m1._moodleUrl, dummyVal('url'), 'Moodle base URL stored');
-        a.equal(m1._token, dummyVal('token'), 'webservice token stored');
-        a.deepEqual(m1._options, dummyOptions, 'options stored');
-        
-        // check URL coercion
-        var m2 = new MoodleWSClient('https://localhost', dummyVal('token'));
-        a.equal(m2._moodleUrl,'https://localhost/', 'trailing / coerced onto Moodle base URL');
+        QUnit.test('options defaults', function(a){
+            a.expect(4);
+            var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'));
+            a.ok(validate.isObject(m1._options), 'options created as object');
+            a.strictEqual(m1._options.acceptUntrustedTLSCert, false, 'acceptUntrustedTLSCert defaults to false');
+            a.strictEqual(m1._options.dataFormat, 'json', "dataFormat defaults to 'json'");
+            a.strictEqual(m1._options.timeout, 5000, "timeout defaults to 5,000ms");
+        });
     });
     
-    QUnit.test('options defaults', function(a){
+    // NOTE
+    // ====
+    // Can't easily test any of the instance methods because they require access
+    // to a Moodle instance, so these tests are very very basic.
+    QUnit.module('instance methods', {}, function(){
+        QUnit.test('.moodleUrl() instance method exists', function(a){
+            a.strictEqual(typeof MoodleWSClient.prototype.moodleUrl, 'function');
+        });
+        QUnit.test('.apiUrl() instance method exists', function(a){
+            a.strictEqual(typeof MoodleWSClient.prototype.apiUrl, 'function');
+        });
+        
+        QUnit.test('.submit() instance method exists', function(a){
+            a.strictEqual(typeof MoodleWSClient.prototype.submit, 'function');
+        });
+        
+        QUnit.test('.registerShortcut() instance method', function(a){
+            a.expect(3);
+            a.strictEqual(typeof MoodleWSClient.prototype.registerShortcut, 'function', 'method exists');
+            var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'));
+            var m2 = m1.registerShortcut('addUser', 'core_user_create_users', 'POST');
+            a.strictEqual(m1, m2, 'returns a reference to self');
+            a.strictEqual(typeof m1.addUser, 'function', 'shortcut function created');
+        });
+        
+        QUnit.test('.registerShortcuts() instance method', function(a){
+            a.expect(4);
+            a.strictEqual(typeof MoodleWSClient.prototype.registerShortcuts, 'function', 'method exists');
+            var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'));
+            var scs = {
+                addUser: ['core_user_create_users', 'POST'],
+                deleteUser: ['core_user_delete_users', 'POST']
+            };
+            var m2 = m1.registerShortcuts(scs);
+            a.strictEqual(m1, m2, 'returns a reference to self');
+            a.strictEqual(typeof m1.addUser, 'function', '1st shortcut function created');
+            a.strictEqual(typeof m1.deleteUser, 'function', '2nd shortcut function created');
+        });
+    });
+    
+    QUnit.test('.encodeWSArguments() static function', function(a){
         a.expect(4);
-        var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'));
-        a.ok(validate.isObject(m1._options), 'options created as object');
-        a.strictEqual(m1._options.acceptUntrustedTLSCert, false, 'acceptUntrustedTLSCert defaults to false');
-        a.strictEqual(m1._options.dataFormat, 'json', "dataFormat defaults to 'json'");
-        a.strictEqual(m1._options.timeout, 5000, "timeout defaults to 5,000ms");
+        a.deepEqual(MoodleWSClient.encodeWSArguments({ 'criteria[0][key]': 'deleted', 'criteria[0][value]': '0' }), { 'criteria[0][key]': 'deleted', 'criteria[0][value]': '0' }, 'already encoded object passes through un-changed');
+        a.deepEqual(MoodleWSClient.encodeWSArguments({a: 'b'}), {a: 'b'}, 'un-nested object returns expected value');
+        a.deepEqual(MoodleWSClient.encodeWSArguments({a: { b: 'c' }}), {'a[b]': 'c'}, 'nested object returns expected value');
+        a.deepEqual(MoodleWSClient.encodeWSArguments({ criteria: [ { key: 'deleted', value: 0 } ] }), { 'criteria[0][key]': 'deleted', 'criteria[0][value]': '0' }, 'nested object containing array returns expected value');
     });
 });
 
-// NOTE
-// ====
-// Can't easily test any of the instance methods because they require access
-// to a Moodle instance, so these tests are very very basic.
-QUnit.module('instance methods', {}, function(){
-    QUnit.test('.moodleUrl() instance method exists', function(a){
-        a.strictEqual(typeof MoodleWSClient.prototype.moodleUrl, 'function');
-    });
-    QUnit.test('.apiUrl() instance method exists', function(a){
-        a.strictEqual(typeof MoodleWSClient.prototype.apiUrl, 'function');
+QUnit.module('custom error class', {}, function(){
+    QUnit.test('class exists', function(a){
+        a.equal(typeof MoodleWSClient.MoodleWSError, 'function');
     });
     
-    QUnit.test('.submit() instance method exists', function(a){
-        a.strictEqual(typeof MoodleWSClient.prototype.submit, 'function');
-    });
-    
-    QUnit.test('.registerShortcut() instance method', function(a){
-        a.expect(3);
-        a.strictEqual(typeof MoodleWSClient.prototype.registerShortcut, 'function', 'method exists');
-        var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'));
-        var m2 = m1.registerShortcut('addUser', 'core_user_create_users', 'POST');
-        a.strictEqual(m1, m2, 'returns a reference to self');
-        a.strictEqual(typeof m1.addUser, 'function', 'shortcut function created');
-    });
-    
-    QUnit.test('.registerShortcuts() instance method', function(a){
-        a.expect(4);
-        a.strictEqual(typeof MoodleWSClient.prototype.registerShortcuts, 'function', 'method exists');
-        var m1 = new MoodleWSClient(dummyVal('url'), dummyVal('token'));
-        var scs = {
-            addUser: ['core_user_create_users', 'POST'],
-            deleteUser: ['core_user_delete_users', 'POST']
+    QUnit.test('response data correctly stored', function(a){
+        a.expect(1);
+        let data = {
+            exception: 'moodle_exception',
+            errorcode: 'categoryidnumbertaken',
+            message: 'ID number is already used for another category'
         };
-        var m2 = m1.registerShortcuts(scs);
-        a.strictEqual(m1, m2, 'returns a reference to self');
-        a.strictEqual(typeof m1.addUser, 'function', '1st shortcut function created');
-        a.strictEqual(typeof m1.deleteUser, 'function', '2nd shortcut function created');
+        let err = new MoodleWSClient.MoodleWSError(data);
+        a.strictEqual(err.responseData(), data);
+    });
+    
+    QUnit.test('messages correctly generated', function(a){
+        a.expect(5);
+        a.strictEqual((new MoodleWSClient.MoodleWSError({})).message, 'unknown webservice error', 'default message on empty data');
+        a.strictEqual((new MoodleWSClient.MoodleWSError({exception: 'moodle_exception'})).message, 'moodle_exception: unknown webservice error', "correct message with just 'moodle_exception'");
+        a.strictEqual((new MoodleWSClient.MoodleWSError({errorcode: 'categoryidnumbertaken'})).message, 'unknown webservice error (Error Code: categoryidnumbertaken)', "correct message with just 'errorcode'");
+        a.strictEqual((new MoodleWSClient.MoodleWSError({message: 'ID number is already used for another category'})).message, 'ID number is already used for another category', "correct message with just 'message'");
+        a.strictEqual(
+            (new MoodleWSClient.MoodleWSError({
+                exception: 'moodle_exception',
+                errorcode: 'categoryidnumbertaken',
+                message: 'ID number is already used for another category'
+            })).message,
+            'moodle_exception: ID number is already used for another category (Error Code: categoryidnumbertaken)',
+            'correct message with all keys passed');
     });
 });
 
-QUnit.test('.encodeWSArguments() static function', function(a){
-    a.expect(4);
-    a.deepEqual(MoodleWSClient.encodeWSArguments({ 'criteria[0][key]': 'deleted', 'criteria[0][value]': '0' }), { 'criteria[0][key]': 'deleted', 'criteria[0][value]': '0' }, 'already encoded object passes through un-changed');
-    a.deepEqual(MoodleWSClient.encodeWSArguments({a: 'b'}), {a: 'b'}, 'un-nested object returns expected value');
-    a.deepEqual(MoodleWSClient.encodeWSArguments({a: { b: 'c' }}), {'a[b]': 'c'}, 'nested object returns expected value');
-    a.deepEqual(MoodleWSClient.encodeWSArguments({ criteria: [ { key: 'deleted', value: 0 } ] }), { 'criteria[0][key]': 'deleted', 'criteria[0][value]': '0' }, 'nested object containing array returns expected value');
-});
+//
+//--- Live tests (only run if moodleDetails.json is present & correct) ---------
+//
+
+if(MOODLE_DETAILS.url){
+    QUnit.module('Live Tests', {}, function(){
+        QUnit.test('webservice ping', function(a){
+            a.expect(2);
+            let m = new MoodleWSClient(MOODLE_DETAILS.url, MOODLE_DETAILS.token);
+            let done = a.async(1);
+            let p = m.submit('GET', 'core_webservice_get_site_info').then(
+                function(data){
+                    a.ok(data && data.siteurl, 'promise resolved to expected data');
+                    done();
+                },
+                function(){ done(); }
+            );
+            a.ok(validate.isPromise(p), '.submit() returns a promise');
+        });
+        
+        QUnit.test('exception catching', function(a){
+            a.expect(3);
+            // use an intentionally incorrect token to trigger an exception
+            let m = new MoodleWSClient(MOODLE_DETAILS.url, '00000000000000000000000000000000');
+            let done = a.async(1);
+            let p = m.submit('GET', 'core_webservice_get_site_info').then(
+                function(){ done(); },
+                function(err){
+                    a.ok(true, 'an error was thrown');
+                    a.ok(err instanceof MoodleWSClient.MoodleWSError, 'the error is a MoodleWSError');
+                    done();
+                }
+            );
+            a.ok(validate.isPromise(p), '.submit() returns a promise');
+        });
+    });
+}
